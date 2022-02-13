0bbbf25bd0cc84c7a7a426604eb3440066ad1026
priyanka <priyankamehra@192.168.0.104>
2022-02-13 20:49:32 +0530

    Update

---
+++ connector-encryptor/src/main/java/com/asimily/ApplicationLoader.java
@@ -0,0 +1,66 @@
+package com.asimily;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.concurrent.Executor;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@Configuration
+@EnableAsync
+public class ApplicationLoader extends SpringBootServletInitializer implements CommandLineRunner {
+	
+	@Value("${priceOne.pound}")
+	private Integer p1pound;
+	
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(ApplicationLoader.class)
+        .build()
+        .run(args);
+  }
+
+  public void run(String... arg0) throws Exception {
+    logger.info("********************entered application...");
+    System.out.println(p1pound);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ApplicationLoader.class);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+
+    };
+  }
+  
+  @PostConstruct
+  public void onApplicationStartUpEvent() {
+    logger.info("********************Starting Application Start Up Activities...");
+  }
+ 
+}
+
